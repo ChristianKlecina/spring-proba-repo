f4fceadfdc8b2a3759834c7167b144e05544e0ee
ChristianKlecina <djfntt@yahoo.com>
2022-08-30 14:09:37 +0200

    first

---
+++ Proba2/src/main/java/com/example/demo/Proba2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proba2Application.class, args);
+	}
+
+}
